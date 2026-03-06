74bc80237a4b69c7f19bb281947e176bf43f4323
hamzahayatunstuck <hamza.hayat@unstucklabs.com>
2026-03-06 20:21:10 +0500

    Add next.config.ts

---
+++ next.config.ts
@@ -0,0 +1,18 @@
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ['typeorm', 'better-sqlite3', 'reflect-metadata'],
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = config.externals || [];
+      if (Array.isArray(config.externals)) {
+        config.externals.push('better-sqlite3');
+      }
+    }
+    return config;
+  },
+};
+
+export default nextConfig;
